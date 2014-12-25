d4945f23a6084a184af4f2c22fd7cda56cd7d54c
Jonathan Rudenberg <jonathan@titanous.com>
2014-12-24 20:30:18 -0500

    Fix concurrency check
    If ConcurrencyLevel is not >= 1, there will be no tokens in the bucket
    for concurrent tests, so they will not run.
    
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- check.go
+++ check.go
@@ -564,7 +564,7 @@ func newSuiteRunner(suite interface{}, runConf *RunConf, concurrent bool, bucket
 	if conf.Benchmark {
 		conf.Verbose = true
 	}
-	if concurrent && conf.ConcurrencyLevel == 0 {
+	if conf.ConcurrencyLevel < 1 {
 		conf.ConcurrencyLevel = 1
 	}
 
